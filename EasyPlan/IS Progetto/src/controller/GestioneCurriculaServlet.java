9f3f15ddb8e5374cf4ed7d62b82d95d97a2c7e4e
cd <cd@c>
2019-01-10 16:43:44 +0100

    Aggiustate 5 classi con checksum

--- EasyPlan/IS Progetto/src/controller/GestioneCurriculaServlet.java
+++ EasyPlan/IS Progetto/src/controller/GestioneCurriculaServlet.java
@@ -12,74 +12,83 @@ import javax.servlet.http.HttpSession;
 
 import model.CurriculumBean;
 import model.CurriculumBeanDAO;
-import model.OffertaFormativaBeanDAO;
 
 /**
- * Servlet implementation class GestioneCurricula
+ * Servlet implementation class GestioneCurricula.
  */
 @WebServlet("/GestioneCurricula")
 public class GestioneCurriculaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GestioneCurriculaServlet() {
-       
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Costruttore vuoto.
+   * @see HttpServlet#HttpServlet()
+   */
+  public GestioneCurriculaServlet() {}
+
+  /**
+   * metodo doGet.
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+    // TODO Auto-generated method stub
+
+    HttpSession session = request.getSession(true);
+    synchronized (session) {
+      if (session.getAttribute("amministratore") 
+          ==  null && session.getAttribute("password") == null) {
+        RequestDispatcher view = request.getRequestDispatcher("Login.html");
+        view.forward(request, response);
+
+      }
+    }
+    if (request.getParameter("metodo").equals("eliminaCurricula")) {
+      int id = Integer.parseInt(request.getParameter("idCurriculum"));
+      System.out.println(id);
+      removeCurriculum(id);
+    }
+    if (request.getParameter("metodo").equals("aggiuntaCurricula")) {
+      int idCorsoDiLaurea = Integer.parseInt(request.getParameter("idCorsoDiLaurea"));
+      String nome = request.getParameter("nomeCurricula");
+      addCurriculum(nome, idCorsoDiLaurea);
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		HttpSession session = request.getSession(true);
-		synchronized (session)
-		{
-			if(session.getAttribute("amministratore") == null && session.getAttribute("password") == null )
-			{
-				RequestDispatcher view = request.getRequestDispatcher("Login.html");
-				view.forward(request, response);
-		
-			}
-		}
-		if(request.getParameter("metodo").equals("eliminaCurricula")) {
-			int id =Integer.parseInt(request.getParameter("idCurriculum"));
-			System.out.println(id);
-			removeCurriculum(id);
-		}
-		if(request.getParameter("metodo").equals("aggiuntaCurricula")) {
-			int idCorsoDiLaurea = Integer.parseInt(request.getParameter("idCorsoDiLaurea"));
-			String nome= request.getParameter("nomeCurricula");
-			addCurriculum(nome,idCorsoDiLaurea);
-		}
-		
-		RequestDispatcher rd = request.getRequestDispatcher("SelezionaCurriculaAm.jsp");
-		String laurea=request.getParameter("laurea");
-		request.setAttribute("laurea", laurea);
-		rd.forward(request, response);
-	}
+    RequestDispatcher rd = request.getRequestDispatcher("SelezionaCurriculaAm.jsp");
+    String laurea = request.getParameter("laurea");
+    request.setAttribute("laurea", laurea);
+    rd.forward(request, response);
+  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-	
-	public void removeCurriculum(int id)throws IOException {
-		CurriculumBeanDAO cbd = new CurriculumBeanDAO();
-		cbd.doDelete(id);
-	}
-	
-	public void addCurriculum(String nome,int IDCorsoDiLAurea)throws IOException {
-		CurriculumBeanDAO cbd = new CurriculumBeanDAO();
-		int id=cbd.doRetrieveByIDMaggiore();
-		CurriculumBean cb= new CurriculumBean(nome,id,IDCorsoDiLAurea,null,null);
-		cbd.doSave(cb);
-	}
+  /**
+   * metodo doPost.
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+
+  public void removeCurriculum(int id) throws IOException {
+    CurriculumBeanDAO cbd = new CurriculumBeanDAO();
+    cbd.doDelete(id);
+  }
+
+  /**
+   * Questo metodo aggiunge un curricula.
+   * @param nome nome del curricula
+   * @param idCorsoDiLaurea id del corso di laurea
+   * @throws IOException viene lanciata l'eccezione IOException
+   */
+  public void addCurriculum(String nome, int idCorsoDiLaurea)
+      throws IOException {
+    CurriculumBeanDAO cbd = new CurriculumBeanDAO();
+    int id = cbd.doRetrieveByIDMaggiore();
+    CurriculumBean cb = new CurriculumBean(nome, id, idCorsoDiLaurea, null, null);
+    cbd.doSave(cb);
+  }
 
 }
--- EasyPlan/IS Progetto/src/controller/GestioneOffertaFormativaServlet.java
+++ EasyPlan/IS Progetto/src/controller/GestioneOffertaFormativaServlet.java
@@ -17,62 +17,73 @@ import model.OffertaFormativaBeanDAO;
 
 @WebServlet("/GestioneOffertaFormativa")
 public class GestioneOffertaFormativaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    public GestioneOffertaFormativaServlet() {
-        super();
+  private static final long serialVersionUID = 1L;
+
+  public GestioneOffertaFormativaServlet() {
+    super();
+  }
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+    HttpSession session = request.getSession(true);
+    synchronized (session) {
+      if (session.getAttribute("amministratore") == null 
+          && session.getAttribute("password") == null) {
+        RequestDispatcher view = request.getRequestDispatcher("Login.html");
+        view.forward(request, response);
+
+      }
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		synchronized (session)
-		{
-			if(session.getAttribute("amministratore") == null && session.getAttribute("password") == null )
-			{
-				RequestDispatcher view = request.getRequestDispatcher("Login.html");
-				view.forward(request, response);
-		
-			}
-		}
-		
-		
-		// Aggiunta Offerta
-		if(request.getParameter("metodo").equals("aggiungiOfferta")) {
-			String nomeOfferta = request.getParameter("nomeOfferta");
-			OffertaFormativaBeanDAO ofbd = new OffertaFormativaBeanDAO();
-			OffertaFormativaBean ofb = new OffertaFormativaBean(nomeOfferta, null, false);
-			ofbd.doSave(ofb);
-			CorsoDiLaureaBeanDAO daoLaurea = new CorsoDiLaureaBeanDAO();
-			int nuovoIdLaureaTriennale = daoLaurea.doRetrieveLastID() +1;
-			CorsoDiLaureaBean corsoDiLaureaTriennale = new CorsoDiLaureaBean(nuovoIdLaureaTriennale,1, nomeOfferta, null);
-			daoLaurea.doSave(corsoDiLaureaTriennale);
-			int nuovoIdLaureaMagistrale = daoLaurea.doRetrieveLastID() +1;
-			CorsoDiLaureaBean corsoDiLaureaMagistrale = new CorsoDiLaureaBean(nuovoIdLaureaMagistrale,2, nomeOfferta, null);
-			daoLaurea.doSave(corsoDiLaureaMagistrale);
-		} else if(request.getParameter("metodo").equals("eliminaOfferta")) {
-			String nomeOfferta = request.getParameter("nomeOfferta");
-			OffertaFormativaBeanDAO ofbd = new OffertaFormativaBeanDAO();
-			ofbd.doDelete(nomeOfferta);
-		} else if(request.getParameter("metodo").equals("visibilita")) {
-			String nomeOfferta = request.getParameter("nomeOfferta");
-			String visibilita = request.getParameter("visibile");
-			boolean visibile = false;
-			if(visibilita.equals("si"))
-				visibile = false;
-			else
-				visibile = true;
-			OffertaFormativaBeanDAO ofbd = new OffertaFormativaBeanDAO();
-			System.out.println("Visibile:" + visibile);
-			OffertaFormativaBean ofb = new OffertaFormativaBean(nomeOfferta, null, visibile);
-			System.out.println( ofbd.doSaveOrUpdate(ofb) );
-		}
-		
-		RequestDispatcher rd = request.getRequestDispatcher("GestioneOfferteFormative.jsp");
-		rd.forward(request, response);
-	}
+    // Aggiunta Offerta
+    if (request.getParameter("metodo").equals("aggiungiOfferta")) {
+      String nomeOfferta = request.getParameter("nomeOfferta");
+      OffertaFormativaBeanDAO ofbd = new OffertaFormativaBeanDAO();
+      OffertaFormativaBean ofb = new OffertaFormativaBean(nomeOfferta, null, false);
+      ofbd.doSave(ofb);
+      
+      CorsoDiLaureaBeanDAO daoLaurea = new CorsoDiLaureaBeanDAO();
+      int nuovoIdLaureaTriennale = daoLaurea.doRetrieveLastID() + 1;
+      
+      CorsoDiLaureaBean corsoDiLaureaTriennale = 
+          new CorsoDiLaureaBean(nuovoIdLaureaTriennale, 1, nomeOfferta, null);
+      daoLaurea.doSave(corsoDiLaureaTriennale);
+      
+      int nuovoIdLaureaMagistrale = daoLaurea.doRetrieveLastID() + 1;
+      
+      CorsoDiLaureaBean corsoDiLaureaMagistrale = 
+          new CorsoDiLaureaBean(nuovoIdLaureaMagistrale, 2, nomeOfferta, null);
+      daoLaurea.doSave(corsoDiLaureaMagistrale);
+      
+    } else if (request.getParameter("metodo").equals("eliminaOfferta")) {
+      String nomeOfferta = request.getParameter("nomeOfferta");
+      OffertaFormativaBeanDAO ofbd = new OffertaFormativaBeanDAO();
+      ofbd.doDelete(nomeOfferta);
+      
+    } else if (request.getParameter("metodo").equals("visibilita")) {
+      String nomeOfferta = request.getParameter("nomeOfferta");
+      String visibilita = request.getParameter("visibile");
+      
+      boolean visibile = false;
+      if (visibilita.equals("si")) {
+        visibile = false;
+      } else {
+        visibile = true;
+      }
+      
+      OffertaFormativaBeanDAO ofbd = new OffertaFormativaBeanDAO();
+      System.out.println("Visibile:" + visibile);
+      OffertaFormativaBean ofb = new OffertaFormativaBean(nomeOfferta, null, visibile);
+      System.out.println(ofbd.doSaveOrUpdate(ofb));
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+    RequestDispatcher rd = request.getRequestDispatcher("GestioneOfferteFormative.jsp");
+    rd.forward(request, response);
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doGet(request, response);
+  }
 
 }
